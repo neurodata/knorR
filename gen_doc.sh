9eb1f8c34f16d6688388ba57416b11543b8c5f00
Disa Mhembere <dmhembe1@jhu.edu>
2017-06-07 16:01:26 -0400

    doc: roxygen is confusing :unamused:

--- gen_doc.sh
+++ gen_doc.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo 'roxygen2::roxygenize("./")' | R --no-save
+#echo 'roxygen2::roxygenize("./")' | R --no-save
+#R -e 'library(roxygen2);roxygenise()'
+R -e 'library(devtools);document()'
